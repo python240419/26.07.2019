477c84b7951014c01014ff65257f3f4753d938f9
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-26 10:35:53 +0300

    Update args_kwargs.py

--- args_kwargs.py
+++ args_kwargs.py
@@ -9,7 +9,7 @@ foo(3, 'hello', 'I' ,'Love','foo')
 
 # targil - write sum method
 #my_sum(5, -6, 11.1, 12)
-def my_sum(*args):
+def my_sum(x, y, *args):
     sum = 0
     for n in args:
         sum = sum + n # or: sum += n
@@ -40,6 +40,33 @@ printDictionary(name='danny',
 #  return- does k appear in dict?
 # *etgar - return a list from kwargs
 
+def statistics(k, **kwargs):
+   print(f'keys = {kwargs.keys()}')
+   print(f'values = {kwargs.values()}')
+   if k in kwargs.keys():
+       print(f'{k} in keys')
+       return True
+   if k in kwargs.values():
+       print(f'{k} in values')
+       return True
+   print(list(kwargs.values()))
+   print(list(kwargs.keys()))
+   listOfAll =\
+       list(kwargs.values())+\
+       list(kwargs.keys())
+   listComb = []
+   for k,v in kwargs.items():
+       listComb.append(k)
+       listComb.append(v)
 
+   return False
+
+statistics('TV', name='danny',
+    age=12, city='TV')
+
+def foo3(x = 7, y = 9, **kwargs):
+    pass
+foo()
+foo(y=8, a=1)
 
 
